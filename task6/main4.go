251c6cbdbe9c5e2215bdb0184d1454a590240653
hirazant <Anton14070@yandex.ru>
2021-12-28 14:47:33 +0300

    add 6,7,8 now

---
+++ task6/main4.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"context"
+	"errors"
+	"fmt"
+	"time"
+)
+
+// Печатает на стандартный вывод и отправляет int в канал
+func longRunningProcess() error {
+	time.Sleep(time.Second)
+	return errors.New("failed: Too long")
+}
+
+func afterLongProcess(ctx context.Context) {
+	select {
+	case <-ctx.Done():
+		fmt.Printf("Last process too long")
+	default:
+		fmt.Printf("Normal mode")
+	}
+}
+
+func main() {
+	ctx := context.Background()
+	ctx, cancel := context.WithCancel(ctx)
+
+	go func() {
+		err := longRunningProcess()
+		if err != nil {
+			cancel()
+		}
+	}()
+	time.Sleep(time.Second)
+	afterLongProcess(ctx)
+}
